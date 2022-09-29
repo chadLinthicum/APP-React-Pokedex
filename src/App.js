5ebcf89d7ffb50d35187e3f5da61c92d5d5a81e0
Chad Linthicum <portableanswers@gmail.com>
2022-09-29 13:15:42 -0400

    First commit. Basic layout done.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>Star Wars</h1>
+      </div>
+      <div className="App-content">
+        <div className="App-content-header">
+          <input placeholder="Name a character in a galaxy far far away..."></input>
+        </div>
+        <div className="App-content-character">
+          <div className="App-content-character-pic">Character Picture</div>
+          <div className="App-content-character-info">Character Info</div>
+        </div>
+        <div className="App-content-navigation">
+          <h3>&#60; &#62;</h3>
+        </div>
+      </div>
     </div>
   );
 }
