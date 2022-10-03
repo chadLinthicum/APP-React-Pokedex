46c31502f472e26a90066628d1b5187077b6d7cc
Chad Linthicum <portableanswers@gmail.com>
2022-10-03 19:02:10 -0400

    Made our first API CALL using a DOG API as a test. Image is rendering for Character Pic DIV

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const axios = require("axios");
+  const [dog, setDog] = useState("");
+
+  function componentDidMount() {
+    axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
+      setDog(response.data.message);
+      console.log(response.data.message);
+    });
+  }
+
+  useEffect(() => {
+    componentDidMount();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
       <div className="App-header">
@@ -12,7 +27,9 @@ function App() {
           <input placeholder="Name a character in a galaxy far far away..."></input>
         </div>
         <div className="App-content-character">
-          <div className="App-content-character-pic">Character Picture</div>
+          <div className="App-content-character-pic">
+            <img id="dog" alt="dog" src={dog} />
+          </div>
           <div className="App-content-character-info">Character Info</div>
         </div>
         <div className="App-content-navigation">
