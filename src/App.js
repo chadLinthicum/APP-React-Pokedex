eb4679067a9eb1abb279349389824d3a9c0960bc
Chad Linthicum <portableanswers@gmail.com>
2022-10-05 14:59:56 -0400

    Showing two types now

--- src/App.js
+++ src/App.js
@@ -8,19 +8,33 @@ function App() {
   const [name, setName] = useState("");
   const [type, setType] = useState("");
   const [weight, setWeight] = useState("");
-  // const [abilities, setAbilities] = useState("");
-  // const [stats, setStats] = useState("");
+  const [hp, setHP] = useState("");
+  const [attack, setAttack] = useState("");
+  const [defense, setDefense] = useState("");
+  const [speed, setSpeed] = useState("");
 
   const fetchData = async () => {
-    const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/6");
+    const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
     console.log(data);
+
     setSprite(data.sprites.other["official-artwork"].front_default);
-    // setSprite(data.sprites.other.home.front_default);
     setNumber(data.id);
     setName(data.name);
-    setType(data.types[0].type.name);
     setWeight(data.weight);
-    console.log(data.types.length);
+    setHP(data.stats[0].base_stat);
+    setAttack(data.stats[1].base_stat);
+    setDefense(data.stats[2].base_stat);
+    setSpeed(data.stats[3].base_stat);
+
+    if (data.types.length > 1) {
+      setType(data.types[0].type.name + "/" + data.types[1].type.name);
+    } else {
+      setType(data.types[0].type.name);
+    }
+
+    for (let i = 0; i < data.types.length; i++) {
+      console.log(data.types[i].type.name);
+    }
   };
 
   useEffect(() => {
@@ -34,7 +48,12 @@ function App() {
       </header>
       <div className="App-content">
         <div className="App-content-header">
-          <input placeholder="Who's that Pokémon!?"></input>
+          <input type="text" list="input" />
+          <datalist id="input">
+            <option>Bulbasaur</option>
+            <option>Ivysaur</option>
+            <option>Venusaur</option>
+          </datalist>
         </div>
         <div className="App-content-character">
           <div className="App-content-character-pic">
@@ -55,6 +74,18 @@ function App() {
                   <td>{name}</td>
                   <td>{type}</td>
                   <td>{weight}</td>
+                </tr>
+                <tr>
+                  <th>HP</th>
+                  <th>Attack</th>
+                  <th>Defense</th>
+                  <th>Speed</th>
+                </tr>
+                <tr>
+                  <td>{hp}</td>
+                  <td>{attack}</td>
+                  <td>{defense}</td>
+                  <td>{speed}</td>
                 </tr>
               </thead>
             </table>
